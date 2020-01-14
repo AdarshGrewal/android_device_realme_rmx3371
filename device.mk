72776f7ac1952c29a1304048a2ba0a3a389bc78e
Leo Liou <leoliou@google.com>
2020-01-14 17:59:51 +0800

    RMX3371: Inherit the virtual a/b config
    For devices launching with virtual A/B, inherit the virtual A/B
    device base configuration.
    
    Bug: 146466421
    Bug: 149155051
    Test: boot to home, check system property of virtual_ab
    Test: Run full storage-qa test
    Change-Id: I8ebbbe897dc9278f43e37a6a9599b89d68419b27
    Signed-off-by: Leo Liou <leoliou@google.com>

--- device.mk
+++ device.mk
@@ -5,6 +5,8 @@
 #
 
 # A/B
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
