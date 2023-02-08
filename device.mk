4c654a1f69b061c15f333844022945c1eb47869c
AdarshGrewal <adarshgrewal@gmail.com>
2023-02-08 17:47:53 +0530

    RMX3371: Stop building tristate KeyHandler

--- device.mk
+++ device.mk
@@ -32,11 +32,6 @@ PRODUCT_PACKAGES += \
     AntHalService-Soong \
     com.dsi.ant@1.0.vendor
 
-# Alert slider
-PRODUCT_PACKAGES += \
-    KeyHandler \
-    tri-state-key-calibrate
-
 # APEX
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
